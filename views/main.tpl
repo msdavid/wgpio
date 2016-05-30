e2df5d6e872059754af88efbf53c939922435dc0
root <mauro@sauco.net>
2016-05-30 17:44:48 +0000

    Completed all functions

--- views/main.tpl
+++ views/main.tpl
@@ -1,99 +1,175 @@
 <!DOCTYPE html>
-<%! UNDEFINED="" %> 
 <html>
 <head>
     <link rel="stylesheet" href="/css/base.css">
     <script src="/js/jquery-2.2.4.min.js"></script>
+    <title> WGPIO v0.2 by Mauro Sauco</title>
 
 </head>
 <body>
+<H2> WGPIO  Ver. 0.2 </H2>
 <table>
+    <tbody>
     %for pin in pins[::2]:
+        <%
+            if pin.channel == 0: ed = ''
+            else: ed = 'contenteditable'
+        %>
         <tr>
             <td class=name_container> 
                 <div class="name">${pin.name}</div>
-                <div pid="${pin.pid}" id="label-${pin.pid}" class="label">CLick</div>
+                <div pid="${pin.pid}" id="label-${pin.pid}" class="label" ${ed}>Click</div>
             </td>
             <td>
                 %if pin.channel > 0:
-                <div pid="${pin.pid}" id="polarity-${pin.pid}" class="polarity right unk"></div>
+                <div pid="${pin.pid}" id="term-${pin.pid}" class="term unk"></div>
                 %endif
             </td>
-            <td><div pid="${pin.pid}" id="device-${pin.pid}"></div></td>
+            <td><div pid="${pin.pid}" id="device-${pin.pid}" class="off"></div></td>
             <td class="pin_container"> 
-                <div id="pin-${pin.pid}" class="pin">${pin.pid}</div>
+                <div id="pin-${pin.pid}" class="pin grey">${pin.pid}</div>
 
-            ## adding one to pin
-            <% pin = pins[pin.pid] %> 
+            ## +1 to pin
+            <% 
+                pin = pins[pin.pid]
+                if pin.channel == 0: ed = ''
+                else: ed = 'contenteditable'
+            %>
             
-                <div id="pin-${pin.pid}" class="pin">${pin.pid}</div>
+                <div id="pin-${pin.pid}" class="pin grey">${pin.pid}</div>
             </td>
-            <td><div pid="${pin.pid}" id="device-${pin.pid}"></div></td>
+            <td><div pid="${pin.pid}" id="device-${pin.pid}" class="off"></div></td>
             <td>
                 %if pin.channel > 0:
-                <div pid="${pin.pid}" id="polarity-${pin.pid}" class="polarity left unk"></div>
+                <div pid="${pin.pid}" id="term-${pin.pid}" class="term unk"></div>
                 %endif
             </td>
             <td class="name_container"> 
                 <div class="name">${pin.name}</div>
-                <div pid="${pin.pid}" id="label-${pin.pid}" class="label">CLick</div>
+                <div pid="${pin.pid}" id="label-${pin.pid}" class="label" ${ed}>Click</div>
             </td> 
         </tr>
     %endfor
+    </tbody>
 </table>
 <script>
-var x;
+ window.pins = null;
     (function poll(){
         $.ajax({ url: "/out", success: function(data){
+            window.pins = data;
             $.each( data, function(index, pin ) {
-                if (pin['label'] != ''){
-                    $("#label-" + pin['pid']).html(pin['label']);
-                };
-                if($("#device-" + pin['pid']).attr('class') != pin['device']){
-                    $("#device-" + pin['pid']).removeClass();
-                    $("#device-" + pin['pid']).addClass(pin['device']);
+                var pid = pin.pid;
+                var label = pin.label;
+                var mode = pin.mode;
+                var status = pin.status;
+                var term = pin.term
+                var elabel = $("#label-" + pid);
+                var eterm = $("#term-" + pid);
+                var edevice = $("#device-" + pid);
+                var epin = $("#pin-" + pid);
+
+                if (mode ==-1) dclass = "";
+                if (mode == 1) dclass = "push";
+                if (mode == 0) dclass = "led";
+                if (term ==-1) tclass = 'unk';
+                if (term == 1) tclass = 'plus';
+                if (term == 0) tclass = 'gnd';
+                if (status ==-1) pclass = 'grey';
+                if (status == 1) pclass = 'red';
+                if (status == 0) pclass = 'green';
+
+                if (!eterm.hasClass(tclass)){
+                    eterm.removeClass();
+                    eterm.addClass('term ' + tclass);
                 };
 
-                if (pin['device'] == "led" && pin['status'] == "on"){
-                    $("#device-" + pin['pid']).addClass("on");
+                if (!epin.hasClass(pclass)){
+                    epin.removeClass();
+                    epin.addClass("pin "+ pclass);
+                }
+
+                if (label != '' && label != elabel.html()){
+                    elabel.html(label);
                 };
+                if(!edevice.hasClass(dclass)){
+                    edevice.removeClass("push","led");
+                    edevice.addClass(dclass);
+                };
+
+                if (mode == 1){
+                    if (status != term && term > -1 && status > -1){
+                        edevice.removeClass("off");
+                        edevice.addClass("on");
+                    }else{
+                        edevice.removeClass("on");
+                        edevice.addClass("off");
+                    }
+                };
+
             });
     },  dataType: "json", complete: poll, timeout: 30000 });
     })();
     
-    $(document).on('mousedown', '.push', function(){
-       console.log(this.id); 
-
-    });
-    $(document).on('mouseup', '.push', function(){
-       console.log("yop"); 
-    });
-
-    $(document).on('mousedown', '.polarity', function(){
+    $(document).on('mousedown', '.push', function(){ 
         obj = $(this);
         id = obj.attr("pid");
-
+        epin = $('#pin-' + id); 
+        term = window.pins[id -1].term;
+        $.ajax({ url: "/in/"+ id +":setstatus:" + term});
+        obj.addClass("press");
+        epin.removeClass();
+        if (term == 0) epin.addClass("pin green");
+        if (term == 1) epin.addClass("pin red");
+        if (term ==-1) epin.addClass("pin grey");
+    });
+    $(document).on('mouseup', '.push', function(){
+        obj = $(this);
+        id = obj.attr("pid");
+        epin = $('#pin-' + id);
+        term = window.pins[id - 1].term;
+        if (term > -1) term = 1 - term;
+        $.ajax({url: "/in/"+ id +":setstatus:" + term});
+        obj.removeClass("press");
+        epin.removeClass();
+        if (term == 0) epin.addClass("pin red");
+        if (term == 1) epin.addClass("pin green");
+        if (term == -1) epin.addClass("pin grey");
+    });
+    $(document).on('mousedown', '.term', function(){
+        obj = $(this);
+        id = obj.attr("pid");
         if ( obj.hasClass('unk') ){
             obj.removeClass('unk');
             obj.addClass('plus');
-            $.ajax({ url: "/in/"+ id +":setpolarity:1"});
+            $.ajax({ url: "/in/"+ id +":setterm:1"});
             return;
        }
         if ( obj.hasClass('plus') ){
             obj.removeClass('plus');
             obj.addClass('gnd');
-            $.ajax({ url: "/in/"+ id +":setpolarity:0"});
+            $.ajax({ url: "/in/"+ id +":setterm:0"});
             return;
        }
         if ( obj.hasClass('gnd') ){
             obj.removeClass('gnd');
             obj.addClass('unk');
-            $.ajax({ url: "/in/"+ id +":setpolarity:-1"});
+            $.ajax({ url: "/in/"+ id +":setterm:-1"});
             return;
        }
+    });
+    $(document).on('focusout','.label',function(){
+        label = $(this).html();
+        if (label == '') label = 'Click'
+        id = $(this).attr('pid')
+        $.ajax({url: "/in/"+ id +":setlabel:" + label});
 
     });
-</script>
+    $(document).on('focusin','.label',function(){
+        label = $(this).html();
+        console.log(label);
+        if (label == "Click") $(this).html("");
+    });
 
+</script>
 </body>
 </html>
--- wgpio.py
+++ wgpio.py
@@ -1,7 +1,7 @@
 from bottle import *
 from bottle import mako_view as view
 import json
-from threading import Thread
+from threading import Thread, Timer
 from time import sleep
 
 REFRESH_RATE = 0.3
@@ -10,66 +10,67 @@ REFRESH_RATE = 0.3
 class Pin:
     def __init__(self, elm):
         self.pid = elm[0]
-        self.device = elm[1]
+        self.channel = elm[1]
         self.name = elm[2]
-        self.status = elm[3]
-        self.label = elm[4]
-        self.polarity = elm[5]
-        self.channel = elm[6]
+        self.label = elm[3]
+        self.status = elm[4]
+        self.pud = False
+        self.term = -1
+        self.mode = -1
+        self.meta = {}
+        self.meta["callbacks"] = []
 
     def asdict(self):
-        return {
-                'pid': self.pid,
-                'device': self.device,
-                'name': self.name,
-                'status': self.status,
-                'label': self.label,
-                'polarity': self.polarity,
+        return {'pid': self.pid,
                 'channel': self.channel,
-            }
+                'name': self.name,
+                'label': self.label,
+                'status': self.status,
+                'pud': self.pud,
+                'term': self.term,
+                'mode': self.mode}
 
-# defaults pin, device, name, status, label, channel_num
 PINS_DEFAULTS = [
-        [1,  'none', '3.3v', '', 'DC Power', -1, 0],
-        [2,  'none', '5v', '', 'DC Power', -1, 0],
-        [3,  'none', 'GPIO-02', -1, '', -1, 2],
-        [4,  'none', '5v', '', 'DC Power', -1, 0],
-        [5,  'none', 'GPIO-03', -1, '', -1, 3],
-        [6,  'none', 'GND', '', 'Ground', -1, 0],
-        [7,  'none', 'GPIO-04', -1, '', -1, 4],
-        [8,  'none', 'GPIO-14', -1, '', -1, 14],
-        [9,  'none', 'GND', '', 'Ground', -1, 0],
-        [10, 'none', 'GPIO-15', -1, '', -1, 15],
-        [11, 'none', 'GPIO-17', -1, '', -1, 17],
-        [12, 'none', 'GPIO-18', -1, '', -1, 18],
-        [13, 'none', 'GPIO-27', -1, '', -1, 27],
-        [14, 'none', 'GND', '', 'Ground', -1, 0],
-        [15, 'none', 'GPIO-22', -1, '', -1, 22],
-        [16, 'none', 'GPIO-23', -1, '', -1, 23],
-        [17, 'none', '3.3v', '', 'DC Power', -1, 0],
-        [18, 'none', 'GPIO-24', -1, '', -1, 24],
-        [19, 'none', 'GPIO-10', -1, '', -1, 10],
-        [20, 'none', 'GND', '', '', -1, 0],
-        [21, 'none', 'GPIO-09', -1, '', -1, 9],
-        [22, 'none', 'GPIO-25', -1, '', -1, 25],
-        [23, 'none', 'GPIO-11', -1, '', -1, 11],
-        [24, 'none', 'GPIO-08', -1, '', -1, 8],
-        [25, 'none', 'GND', '', 'Ground', -1, 0],
-        [26, 'none', 'GPIO-07', -1, '', -1, 7],
-        [27, 'none', 'ID_SD', '', '', -1, 0],
-        [28, 'none', 'ID_SC', '', '', -1, 0],
-        [29, 'none', 'GPIO-05', -1, '', -1, 5],
-        [30, 'none', 'GND', '', 'Ground', -1, 0],
-        [31, 'none', 'GPIO-06', -1, '', -1, 6],
-        [32, 'none', 'GPIO-12', -1, '', -1, 12],
-        [33, 'none', 'GPIO-13', -1, '', -1, 13],
-        [34, 'none', 'GND', '', 'Ground', -1, 0],
-        [35, 'none', 'GPIO-19', -1, '', -1, 19],
-        [36, 'none', 'GPIO-16', -1, '', -1, 16],
-        [37, 'none', 'GPIO-26', -1, '', -1, 26],
-        [38, 'none', 'GPIO-20', -1, '', -1, 20],
-        [39, 'none', 'GND', '', 'Ground', -1, 0],
-        [40, 'none', 'GPIO-21', -1, '', -1, 21],
+        [1,     0, '3.3v',     'DC Power', 1],
+        [2,     0, '5v',       'DC Power', 1],
+        [3,     2, 'GPIO-02',  '',        -1],
+        [4,     0, '5v',       'DC Power', 1],
+        [5,     3, 'GPIO-03',  '',        -1],
+        [6,     0, 'GND',      'Ground',   0],
+        [7,     4, 'GPIO-04',  '',        -1],
+        [8,    14, 'GPIO-14',  '',        -1],
+        [9,     0, 'GND',      'Ground',   0],
+        [10,   15, 'GPIO-15',  '',        -1],
+        [11,   17, 'GPIO-17',  '',        -1],
+        [12,   18, 'GPIO-18',  '',        -1],
+        [13,   27, 'GPIO-27',  '',        -1],
+        [14,    0, 'GND',      'Ground',   0],
+        [15,   22, 'GPIO-22',  '',        -1],
+        [16,   23, 'GPIO-23',  '',        -1],
+        [17,    0, '3.3v',     'DC Power', 1],
+        [18,   24, 'GPIO-24',  '',        -1],
+        [19,   10, 'GPIO-10',  '',        -1],
+        [20,    0, 'GND',      '',         0],
+        [21,    9, 'GPIO-09',  '',        -1],
+        [22,   25, 'GPIO-25',  '',        -1],
+        [23,   11, 'GPIO-11',  '',        -1],
+        [24,    8, 'GPIO-08',  '',        -1],
+        [25,    0, 'GND',      'Ground',   0],
+        [26,    7, 'GPIO-07',  '',        -1],
+        [27,    0, 'ID_SD',    '-',       -1],
+        [28,    0, 'ID_SC',    '-',       -1],
+        [29,    5, 'GPIO-05',  '',        -1],
+        [30,    0, 'GND',      'Ground',   0],
+        [31,    6, 'GPIO-06',  '',        -1],
+        [32,   12, 'GPIO-12',  '',        -1],
+        [33,   13, 'GPIO-13',  '',        -1],
+        [34,    0, 'GND',      'Ground',   0],
+        [35,   19, 'GPIO-19',  '',        -1],
+        [36,   16, 'GPIO-16',  '',        -1],
+        [37,   26, 'GPIO-26',  '',        -1],
+        [38,   20, 'GPIO-20',  '',        -1],
+        [39,    0, 'GND',      'Ground',   0],
+        [40,   21, 'GPIO-21',  '',        -1],
     ]
 
 PINS = [Pin(elm=e) for e in PINS_DEFAULTS]
@@ -117,14 +118,15 @@ def process(cmd):
     commands = cmd.split(',')
     for command in commands:
         target, action, data = command.split(':')
-        if action == 'sethigh':
-            GWGPIO.bypin(int(target)).status = GWGPIO.HIGH
-        if action == 'setlow':
-            GWGPIO.bypin(int(target)).status = GWGPIO.LOW
+        if action == 'setstatus':
+            GWGPIO.bypin(int(target)).status = int(data)
+            return "set pin # %s status to %s: OK" % (target, data)
         if action == "setlabel":
-            GWGPIO.bypin(int(target)).label = int(data)
-        if action == "setpolarity":
-            GWGPIO.bypin(int(target)).polarity = int(data)
+            GWGPIO.bypin(int(target)).label = data
+            return "set pin # %s label to %s: OK" % (target, data)
+        if action == "setterm":
+            GWGPIO.bypin(int(target)).term = int(data)
+            return "set pin # %s term to %s: OK" % (target, data)
 
 
 def runweb():
@@ -157,10 +159,9 @@ class WGPIO:
         self.PUD_OFF = 20
         self.PUD_DOWN = 21
         self.BOARD = 10
-        self.pins = PINS
         self.MODE = None
-        self.PUSH = 'push'
-        self.LED = 'led'
+        self.pins = PINS
+        self.WARNINGS = True
         GWGPIO = self  # I have the feeling that this is an ugly approach :(
 
     def gpio(self, channel):
@@ -186,29 +187,93 @@ class WGPIO:
         else:
             raise Error('Pin %s is not an IO number' % number)
 
-    def event_detected(*k):
-        pass
+    def remove_event_detect(self, channel):
+        pin = self.gpio(channel)
+        pin.meta["detect_active"] = False
+        pin.meta["callbacks"] = []
 
-    def remove_event_detect(*k):
-        pass
+    def add_event_callback(self, channel, callback):
+        pin = self.gpio(channel)
+        if not pin.meta["detect_active"]:
+            raise Error("Add event detection using add_event_detect "
+                        "first before adding a callback")
+        pin.meta["callbacks"].append(callback)
 
-    def add_event_callback(*k):
-        pass
-
-    def gpio_function(*k):
-        pass
+    def gpio_function(self, channel):
+        return self.gpio(channel).mode
 
     def setup(self, channels, direction, pull_up_down=20, initial=0):
         if type(channels) is not list:
             channels = [channels]
         for channel in channels:
-            if direction == self.OUT:
-                self.gpio(channel).device = self.LED
-            if direction == self.IN:
-                self.gpio(channel).device = self.PUSH
+            self.gpio(channel).mode = direction
+            if pull_up_down == self.PUD_UP:
+                self.gpio(channel).polarity = 0
+            if pull_up_down == self.PUD_DOWN:
+                self.gpio(channel).status = 0
 
-    def wait_for_edge(*k):
-        pass
+    def wait_for_edge(self, channel, edge, bouncetime=None, timeout=None):
+        # bouncetime its irrelevant on soft implementation (I hope)
+        pin = self.gpio(channel)
+        laststatus = pin.status
+        pin.meta["timeout"] = False
+        pin.meta["detect_active"] = True
+
+        def dotimeout():
+            pin.meta["timeout"] = True
+
+        if timeout:
+            Timer(float(timeout)/1000, dotimeout).start()
+
+        while True:
+            if pin.meta["timeout"] or not pin.meta["detect_active"]:
+                pin.meta["timeout"] = False
+                return
+            sleep(0.01)  # <- might be too long
+            if edge == self.RISING and pin.status == self.HIGH:
+                break
+            if edge == self.FALLING and pin.status == self.LOW:
+                break
+            if edge == self.BOTH and pin.status != laststatus:
+                # means EITHER? .. I am not sure
+                break
+            laststatus = pin.status
+        return channel
+
+    def add_event_detect(self, channel, edge, callback=None, bouncetime=None):
+        pin = self.gpio(channel)
+        if pin.mode != 1:
+            raise Error("You must setup() the GPIO channel as an input first")
+        pin.meta["edge_detected"] = False
+        pin.meta["detect_active"] = True
+        if callback:
+            pin.meta["callbacks"].append(callback)
+
+        def watcher():
+            if not pin.meta["detect_active"]:
+                return
+            while pin.meta["detect_active"]:
+                if self.wait_for_edge(channel, edge) == channel:
+                    pin.meta["edge_detected"] = True
+                    if len(pin.meta["callbacks"]) > 0:
+                        for cb in pin.meta["callbacks"]:
+                            cb(channel)
+                if bouncetime:
+                    sleep(float(bouncetime)/1000)
+                    # not sure if I got the bouncetime concept correctly
+
+        t = Thread(target=watcher)
+        t.daemon = True
+        t.start()
+
+    def event_detected(self, channel):
+        pin = self.gpio(channel)
+        if not pin.meta["detect_active"]:
+            raise Error("no event active on this channel")
+        if pin.meta["edge_detected"]:
+            pin.meta["edge_detected"] = False
+            return True
+        return False
 
     def cleanup(self, channels=False):
         if not channels:
@@ -226,17 +291,20 @@ class WGPIO:
             channels = [channels]
         for channel in channels:
             pin = self.gpio(channel)
-            if pin.device == "none":
-                raise Error("Channel %s is not setup" % channel)
+            if pin.mode == -1:
+                raise Error("You must setup() the GPIO channel first ")
             if value == self.HIGH:
-                pin.status = "on"
+                pin.status = 1
             elif value == self.LOW:
-                pin.status = "off"
+                pin.status = 0
             else:
-                raise Error("%s is an invalid value" % value)
+                raise Error("The channel sent is invalid on a Raspberry Pi")
 
-    def input(*k):
-        pass
+    def input(self, channel):
+        if self.gpio(channel).mode != 0:
+            return self.gpio(channel).status
+        else:
+            raise Error("You must setup() the GPIO channel first")
 
     def setwarnings(*k):
         pass
@@ -246,9 +314,6 @@ class WGPIO:
 
     def setmode(self, mode):
         self.MODE = mode
-
-    def add_event_detect(*k):
-        pass
 
 
 class Error(Exception):
