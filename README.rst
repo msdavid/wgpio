a75de7d895932792193f24ac5bbe7d3e69bdb958
Mauro <mauro@sauco.net>
2016-06-01 18:21:31 +0800

    adding setup.py installation

---
+++ README.rst
@@ -0,0 +1,13 @@
+WGPIO
+-----
+
+To use do:
+
+>>> from wgpio import WGPIO
+>>> GPIO = WGPIO()
+
+is a drop-in replacement for RPi.GPIO
+
+go to your browser to see the simulated raspberry pi piout simulation
+and browse http://<ip-address>:8000  (ie http://localhost:8000 or http://192.168.1.10:8000)
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+        with open('README.rst') as f:
+                    return f.read()
+
+setup(name='wgpio',
+      version='0.1',
+      description='Rpi.GPIO simulation with winterface',
+      long_description=readme(),
+      url='http://github.com/msdavid/wgpio',
+      author='Mauro D. Sauco',
+      author_email='mauro@sauco.net',
+      license='MIT',
+      packages=['.'],
+      install_requires=['mako','cherrypy'],
+      include_package_data=True,
+      keywords='GPIO WGPIO simulation mauro sauco',
+      zip_safe=False)
